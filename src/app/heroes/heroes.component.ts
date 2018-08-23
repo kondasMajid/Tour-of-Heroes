92a30308b87e953d75da42c9f10febda7b215ff9
kondasMajid <kondasmajid@gmail.com>
2018-08-22 22:01:07 -0700

    Displaying List of Details from Dataset

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -7,18 +7,17 @@ import { HEROES } from '../mock-heroes'
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  // selectedHero: Hero = {
-  // id: 1,
-  // name: 'Windstorm',
+  heroes = HEROES;
 
-  // heroes = HEROES;
   selectedHero: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
+
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
